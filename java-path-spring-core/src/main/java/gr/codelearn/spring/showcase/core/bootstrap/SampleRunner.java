1298bfab184b4710b8ad7c4032230b6f43aa7ea5
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2021-10-14 13:09:00 +0300

    Showcasing the use of Spring profiles, profile specific properties and default values.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/bootstrap/SampleRunner.java
+++ java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/bootstrap/SampleRunner.java
@@ -3,6 +3,7 @@ package gr.codelearn.spring.showcase.core.bootstrap;
 import gr.codelearn.spring.showcase.core.base.AbstractLogComponent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
  * application initialization.
  */
 @Component
+@Profile("sample")
 public class SampleRunner extends AbstractLogComponent implements CommandLineRunner {
 	@Value("${spring.application.name}")
 	private String applicationName;
@@ -17,9 +19,12 @@ public class SampleRunner extends AbstractLogComponent implements CommandLineRun
 	@Value("${custom.property}")
 	private String customProperty;
 
+	@Value("${sample.property:0}")
+	private int sampleProperty;
+
 	@Override
 	public void run(final String... args) {
-		logger.info("Application named '{}' has a custom property having the value of '{}'.", applicationName,
-					customProperty);
+		logger.info("Application named '{}' has a custom property having the value of '{}' and a profile based " +
+							"property having the value of '{}'.", applicationName, customProperty, sampleProperty);
 	}
 }
