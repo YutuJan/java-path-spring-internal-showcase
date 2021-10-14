3971f909642293c6ae158c7f2df8f90200cbef0a
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2021-10-14 13:06:40 +0300

    Showcasing Java Configuration option.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

---
+++ java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/config/SampleConfig.java
@@ -0,0 +1,14 @@
+package gr.codelearn.spring.showcase.core.config;
+
+import gr.codelearn.spring.showcase.core.component.IndependentBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SampleConfig {
+	@Bean
+	public IndependentBean getIndependentBean() {
+
+		return new IndependentBean();
+	}
+}
