48033460e088d1fd635a10731f12da42014f068b
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2021-11-05 17:28:52 +0200

    Showcased various techniques to write both JPSQL and SQL queries while demonstrating how to externalize them and define DTOs as custom projections.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/repository/ProductRepository.java
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/repository/ProductRepository.java
@@ -11,4 +11,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	@Query("select p from Product p join fetch p.category")
 	List<Product> findAllLazy();
+
+	Product findBySerial(String product);
 }
