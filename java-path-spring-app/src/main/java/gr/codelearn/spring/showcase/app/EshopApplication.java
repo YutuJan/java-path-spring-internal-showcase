d9c87d50171b26877e36c52fc409694a8dc19d00
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2021-11-05 17:27:30 +0200

    Renamed main application class.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/EshopApplication.java
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/EshopApplication.java
@@ -2,13 +2,15 @@ package gr.codelearn.spring.showcase.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Spring Boot application initializer.
  */
 @SpringBootApplication
-public class Application {
+@EnableJpaRepositories(namedQueriesLocation = "classpath:jpa-named-queries.properties")
+public class EshopApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(EshopApplication.class, args);
 	}
 }
